108e0d76010f5929859498f89295ea64526802bc
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2022-03-27 20:36:22 -0600

    Update form class
    - Add new param on input
    - Add new input personalize

--- modelo/form.class.php
+++ modelo/form.class.php
@@ -4,10 +4,10 @@
 //Se usa para generar los input básicos de un formulario.
 
 class Formulario{
-    //Textbox
-    //Propiedades: Nombre e ID, Placeholder
+    //Textbox ---------------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, Label, Placeholder
     //Propiedades opcionales(Para usarlas, colocar 1 en su parametro): Required, Autofocus 
-    public function textbox($name, $placeholder, $required = "", $autofocus = ""){
+    public function textbox($name, $label, $placeholder, $required = "", $autofocus = ""){
         //Required
         if($required == 1){
             $required = "required";
@@ -20,22 +20,22 @@ class Formulario{
 
         //Input
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <input type="text" name="$name" id="$name" placeholder="$placeholder" $required $autofocus>
-</div>\n
-DFM;
+        <div class="contenedor-input">
+            <div class="contenedor-label">
+                <label for="$name">$label</label>
+            </div>
+            <input type="text" name="$name" id="$name" placeholder="$placeholder" title="$placeholder" $required $autofocus>
+        </div>\n
+        DFM;
 
         return $input;
     }
 
-    //Number
-    //Propiedades: Nombre e ID, Placeholder
+    //Number ------------------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, Label, Placeholder
     //Propiedades opcionales(Colocar 1 para activarlas): Required, Autofocus
     //Propiedades opcionales(Colocar sus valores en el call): Valor mínimo, Valor máximo, Step 
-    public function number($name, $placeholder, $required = "", $autofocus = "", $min = "", $max = "", $step = ""){
+    public function number($name, $label, $placeholder, $required = "", $autofocus = "", $min = "", $max = "", $step = ""){
         //Required
         if($required == 1){
             $required = "required";
@@ -46,24 +46,39 @@ DFM;
             $autofocus = "autofocus";
         }
 
+        //Minimo
+        if(is_numeric($min)){
+            $min = "min='$min'";
+        }
+
+        //Maximo
+        if(is_numeric($max)){
+            $max = "max='$max'";
+        }
+
+        //Setp
+        if(is_numeric($step)){
+            $step = "step='$step'";
+        }
+
         //Input
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <input type="number" name="$name" id="$name" min="$min" max="$max" step="$step" placeholder="$placeholder" $required $autofocus>
-</div>\n
-DFM;
+        <div class="contenedor-input">
+            <div class="contenedor-label">
+                <label for="$name">$label</label>
+            </div>
+            <input type="number" name="$name" id="$name" $min $max $step placeholder="$placeholder" title="$placeholder" $required $autofocus>
+        </div>\n
+        DFM;
 
         return $input;
     }
 
-    //Textarea
-    //Propiedades: Nombre e ID, Placeholder
+    //Textarea ----------------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, label, Placeholder
     //Propiedades opcionales(Colocar 1 para activarlas): Required, Autofocus
     //Propiedades opcionales(Colocar valores para activarlos): Número de columnas, Números de filas,
-    public function textarea($name, $placeholder, $required = "", $autofocus = "", $cols = 30, $rows = 10){
+    public function textarea($name, $label, $placeholder, $required = "", $autofocus = "", $cols = 30, $rows = 10){
         //Required
         if($required == 1){
             $required = "required";
@@ -76,21 +91,21 @@ DFM;
 
         //Input
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <textarea name="$name" id="$name" cols="$cols" rows="$rows"$required, $autofocus></textarea>
-</div>\n
-DFM;
+        <div class="contenedor-textarea">
+            <div class="contenedor-label">
+                <label for="$name">$label</label>
+            </div>
+            <textarea name="$name" id="$name" cols="$cols" rows="$rows" title="$placeholder" $required $autofocus>$placeholder</textarea>
+        </div>\n
+        DFM;
 
         return $input;
     }
 
-    //Date
-    //Propiedades: Nombre e ID
+    //Date --------------------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, Descripcion del input, Tooltip
     //Propiedades opcionales(Coloca 1 para activarlo): Required, Autofocus
-    public function date($name, $required = "", $autofocus = ""){
+    public function date($name, $label, $tooltip, $required = "", $autofocus = ""){
         //Required
         if($required == 1){
             $required = "required";
@@ -103,36 +118,36 @@ DFM;
 
         //Input
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <input type="date" name="$name" id="$name"$required $autofocus>
-</div>\n
-DFM;
+        <div class="contenedor-input">
+            <div class="contenedor-label">
+                <label for="$name">$label</label>
+            </div>
+            <input type="date" name="$name" id="$name" title="$tooltip" $required $autofocus>
+        </div>\n
+        DFM;
 
         return $input;
     }
 
-    //Datetime
+    //Datetime ---------------------------------------------------------------------------------------
     //Sin funcionar
     public function datetime($name){
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <input type="datetime" name="$name" id="$name">
-</div>\n
-DFM;
+        <div class="contenedor-input">
+            <div class="contenedor-label">
+                <label for="$name">$name</label>
+            </div>
+            <input type="datetime" name="$name" id="$name">
+        </div>\n
+        DFM;
 
         return $input;
     }
 
-    //Select
-    //Propiedades: Nombre e ID, Opciones, Placeholder
+    //Select ----------------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, $label, Opciones, Placeholder
     //Propiedades opcionales(Colocar 1 para activarlo): Required, Autofocus
-    public function select($name, $opciones, $placeholder, $required = "", $autofocus = ""){
+    public function select($name, $label, $opciones, $placeholder, $required = "", $autofocus = ""){
         //Required
         if($required == 1){
             $required = "required";
@@ -151,16 +166,40 @@ DFM;
 
         //Input
         $input =<<<DFM
-<div class="contenedor-input">
-    <div class="contenedor-label">
-        <label for="$name">$name</label>
-    </div>
-    <select name="$name" id="$name"$required $autofocus>
-        <option value="">$placeholder</option>
-        $options;
-    </select>
-</div>\n
-DFM;
+        <div class="contenedor-input">
+            <div class="contenedor-label">
+                <label for="$name">$label</label>
+            </div>
+            <select name="$name" id="$name" title="$placeholder" $required $autofocus>
+                <option value="">$placeholder</option>
+                $options;
+            </select>
+        </div>\n
+        DFM;
+
+        return $input;
+    }
+
+    //Textbox Personalizado: Titulo --------------------------------------------------------------------------------
+    //Propiedades: Nombre e ID, Placeholder
+    //Propiedades opcionales(Para usarlas, colocar 1 en su parametro): Required, Autofocus 
+    public function textboxPersonalizado_Titulo($name, $tooltip, $value, $required = "", $autofocus = ""){
+        //Required
+        if($required == 1){
+            $required = "required";
+        }
+
+        //Autofocus
+        if($autofocus == 1){
+            $autofocus = "autofocus";
+        }
+
+        //Input
+        $input =<<<DFM
+        <div class="form-header-titulo">
+            <input type="text" name="$name" id="$name" title="$tooltip" value="$value" $required $autofocus>
+        </div>\n
+        DFM;
 
         return $input;
     }
